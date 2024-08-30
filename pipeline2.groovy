3ed0d0ac9d4389ca82aa1865c3d3b59176242cd0
vidhi-joshi4 <s223374053@deakin.edu.au>
2024-08-30 13:57:04 +1000

    Update pipeline2.groovy

--- pipeline2.groovy
+++ pipeline2.groovy
@@ -3,9 +3,7 @@ pipeline
     agent any 
     environment 
     {
-        DIRECTORY_PATH= "The path of the code directory"
-        TESTING_ENVIRONMENT= "This testing environment"
-        PRODUCTION_ENVIRONMENT= "Vidhi"
+       // none required
     }
     stages
     {
@@ -13,30 +11,61 @@ pipeline
         {
             steps
             {
-                echo "$DIRECTORY_PATH is being fetched from $TESTING_ENVIRONMENT"
-                echo "compile the code and generate any necessary artifacts"
+                echo "Build using Maven to compile and package"
             }
         }
         stage('Unit and Integration Tests')
         {
             steps
             {
-                echo "Unit tests"
-                echo "Integration tests"
+                echo "Unit tests: Ensuring code functions as expected"
+                echo "Integration tests: Ensuring different components of the application work together as expected"
+                echo "Unit and Integration tests are conducted using Appium"
+
             }
+            post
+            {
+                success
+                {
+                    mail to: "jenkinsdeakin@gmail.com"
+                    subject: "Unit tests and Integration tests outcome",
+                    body: "Unit tests and Integration test have been successful"
+                }
+            }
+            
         }
         stage('Code Analysis')
         {
             steps
             {
-                echo "Check the quality of the code"
+                echo "Analysing Code to ensure it meets industry standards."
+                echo "Code Analysed using SonarQube"
             }
+            
         }
         stage('Security Scan')
         {
             steps
             {
-                echo "The application will be deployed to $TESTING_ENVIRONMENT as specified by the environment variable $PRODUCTION_ENVIRONMENT"
+                echo "Conducting security scan"
+                echo "Security scan completed using Burp Suite"
+            }
+            post
+            {
+                success
+                {
+                    mail to: "jenkinsdeakin@gmail.com"
+                    subject: "Security Scan outcome",
+                    body: "Security Scan was successful"
+                }
+            }
+        }
+        stage('Deploy to Staging')
+        {
+            steps
+            {
+                echo "Application will be deployed to staging server"
+                echo "Staging server is AWS EC2"
             }
         }
         stage('Integration Tests on Staging')
@@ -44,7 +73,7 @@ pipeline
             steps
             {
                 
-                    sleep 10
+                 echo "Running integration tests on the staging environment to ensure the application functions as expected in a production-like environment."
                 
             }
         }
@@ -52,15 +81,8 @@ pipeline
         {
             steps
             {
-                echo "Code is being deployed to this production environment: $PRODUCTION_ENVIRONMENT"
-            }
-        }
-
-        stage('completing final')
-        {
-            steps
-            {
-                echo "completing"
+                echo "Deploying the application"
+                echo "Production Server used is AWS EC2"
             }
         }
     }
