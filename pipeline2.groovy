5ff9f008c124402b8ea4b52648e632531f30b133
vidhi-joshi4 <s223374053@deakin.edu.au>
2024-09-05 17:13:12 +1000

    Update pipeline2.groovy

--- pipeline2.groovy
+++ pipeline2.groovy
@@ -1,83 +1,72 @@
-pipeline
-{
+pipeline {
     agent any 
-   
-    stages
-    {
-        stage("Build")
-        {
-            steps
-            {
+
+    stages {
+        stage("Build") {
+            steps {
                 echo "Build using Maven to compile and package"
             }
         }
-        stage("Unit and Integration Tests")
-        {
-            steps
-            {
+        stage("Unit and Integration Tests") {
+            steps {
                 echo "Unit tests: Ensuring code functions as expected"
                 echo "Integration tests: Ensuring different components of the application work together as expected"
                 echo "Unit and Integration tests are conducted using Appium"
-
             }
-            post
-            {
-                success
-                {
-                    mail to: "jenkinsdeakin@gmail.com",
-                    subject: "Unit tests and Integration tests outcome",
-                    body: "Unit tests and Integration test have been successful"
-                    
+            post {
+                success {
+                    script {
+                        def buildLog = currentBuild.rawBuild.logFile.text
+                        emailext(
+                            to: "jenkinsdeakin@gmail.com",
+                            subject: "Unit tests and Integration tests outcome",
+                            body: "Unit tests and Integration tests have been successful",
+                            attachmentsPattern: "build.log"
+                        )
+                        writeFile file: 'build.log', text: buildLog
+                    }
                 }
             }
-            
         }
-        stage("Code Analysis")
-        {
-            steps
-            {
+        stage("Code Analysis") {
+            steps {
                 echo "Analysing Code to ensure it meets industry standards."
                 echo "Code Analysed using SonarQube"
             }
-            
         }
-        stage("Security Scan")
-        {
-            steps
-            {
+        stage("Security Scan") {
+            steps {
                 echo "Conducting security scan"
                 echo "Security scan completed using Burp Suite"
             }
-            post
-            {
-                success
-                {
-                    mail to: "jenkinsdeakin@gmail.com",
-                    subject: "Security Scan outcome",
-                    body: "Security Scan was successful"
-                    
+            post {
+                success {
+                    script {
+                        def buildLog = currentBuild.rawBuild.logFile.text
+                        emailext(
+                            to: "jenkinsdeakin@gmail.com",
+                            subject: "Security Scan outcome",
+                            body: "Security Scan was successful",
+                            attachmentsPattern: "build.log"
+                        )
+                        writeFile file: 'build.log', text: buildLog
+                    }
                 }
             }
         }
-        stage("Deploy to Staging")
-        {
-            steps
-            {
+        stage("Deploy to Staging") {
+            steps {
                 echo "Application will be deployed to staging server"
                 echo "Staging server is AWS EC2"
             }
         }
-        stage("Integration Tests on Staging")
-        {
-            steps
-            {
-                 echo "Running integration tests on the staging environment to ensure the application functions as expected in a production-like environment."
+        stage("Integration Tests on Staging") {
+            steps {
+                echo "Running integration tests on the staging environment to ensure the application functions as expected in a production-like environment."
             }
         }
-        stage("Deploy to Production")
-        {
-            steps
-            {
+        stage("Deploy to Production") {
+            steps {
                 echo "Deploying the application"
                 echo "Production Server used is AWS EC2"
             }
