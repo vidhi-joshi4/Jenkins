3a1eabf4c6da11e2b659c3db71270c9bbb3cd9ad
vidhi-joshi4 <s223374053@deakin.edu.au>
2024-09-05 17:41:52 +1000

    Update pipeline2.groovy

--- pipeline2.groovy
+++ pipeline2.groovy
@@ -1,79 +1,49 @@
 pipeline {
-    agent any
-
-    stages {
-        stage("Build") {
-            steps {
-                echo "Build using Maven to compile and package"
-            }
-        }
-        stage("Unit and Integration Tests") {
-            steps {
-                echo "Unit tests: Ensuring code functions as expected"
-                echo "Integration tests: Ensuring different components of the application work together as expected"
-                echo "Unit and Integration tests are conducted using Appium"
-            }
-            post {
-                success {
-                    script {
-                        // Save build log to a file
-                        sh "cat \${JENKINS_HOME}/jobs/\${JOB_NAME}/builds/\${BUILD_NUMBER}/log > build.log"
-                        
-                        // Send email with attachment
-                        emailext (
-                            to: "jenkinsdeakin@gmail.com",
-                            subject: "Unit tests and Integration tests outcome",
-                            body: "Unit tests and Integration tests have been successful",
-                            attachmentsPattern: "build.log"
-                        )
-                    }
-                }
-            }
-        }
-        stage("Code Analysis") {
-            steps {
-                echo "Analysing Code to ensure it meets industry standards."
-                echo "Code Analysed using SonarQube"
-            }
-        }
-        stage("Security Scan") {
-            steps {
-                echo "Conducting security scan"
-                echo "Security scan completed using Burp Suite"
-            }
-            post {
-                success {
-                    script {
-                        // Save build log to a file
-                        sh "cat \${JENKINS_HOME}/jobs/\${JOB_NAME}/builds/\${BUILD_NUMBER}/log > build.log"
-                        
-                        // Send email with attachment
-                        emailext (
-                            to: "jenkinsdeakin@gmail.com",
-                            subject: "Security Scan outcome",
-                            body: "Security Scan was successful",
-                            attachmentsPattern: "build.log"
-                        )
-                    }
-                }
-            }
-        }
-        stage("Deploy to Staging") {
-            steps {
-                echo "Application will be deployed to staging server"
-                echo "Staging server is AWS EC2"
-            }
-        }
-        stage("Integration Tests on Staging") {
-            steps {
-                echo "Running integration tests on the staging environment to ensure the application functions as expected in a production-like environment."
-            }
-        }
-        stage("Deploy to Production") {
-            steps {
-                echo "Deploying the application"
-                echo "Production Server used is AWS EC2"
-            }
-        }
-    }
+agent any environment {
+// none required
+} stages {
+stage('Build') {
+steps {
+echo "Build using Maven to compile and package" }
+}
+stage('Unit and Integration Tests') {
+steps {
+echo "Unit tests: Ensuring code functions as expected"
+echo "Integration tests: Ensuring different components of the application work together as expected"
+echo "Unit and Integration tests are conducted using Appium"
+} post {
+success {
+mail to: "jenkinsdeakin@gmail.com"
+subject: "Unit tests and Integration tests outcome",
+body: "Unit tests and Integration test have been successful"
+} }
+}
+stage('Code Analysis') {
+steps {
+echo "Analysing Code to ensure it meets industry standards."
+echo "Code Analysed using SonarQube" }
+}
+    stage('Security Scan') {
+steps {
+echo "Conducting security scan"
+echo "Security scan completed using Burp Suite" }
+post {
+success {
+mail to: "jenkinsdeakin@gmail.com", subject: "Security Scan outcome", body: "Security Scan was successful"
+} }
+}
+stage('Deploy to Staging') {
+steps {
+echo "Application will be deployed to staging server"
+echo "Staging server is AWS EC2" }
+}
+stage('Integration Tests on Staging') {
+steps {
+echo "Running integration tests on the staging environment to ensure the application functions as expected in a production-like environment."
+} }
+stage('Deploy to Production') {
+steps {
+echo "Deploying the application"
+echo "Production Server used is AWS EC2" }
+} }
 }
