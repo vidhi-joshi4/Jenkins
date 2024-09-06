80396acc2eda88b74e1c1476fed11dd896b07d81
vidhi-joshi4 <s223374053@deakin.edu.au>
2024-09-06 22:03:50 +1000

    Update pipeline2.groovy

--- pipeline2.groovy
+++ pipeline2.groovy
@@ -1,129 +1,86 @@
-pipeline
-{
+pipeline {
     agent any 
     
-    stages
-    {
-        stage('Build')
-        {
-            steps
-            {
+    stages {
+        stage('Build') {
+            steps {
                 echo "Build using Maven to compile and package"
             }
         }
-        stage('Unit and Integration Tests')
-        {
-            steps
-            {
+        stage('Unit and Integration Tests') {
+            steps {
                 echo "Unit tests: Ensuring code functions as expected"
                 echo "Integration tests: Ensuring different components of the application work together as expected"
                 echo "Unit and Integration tests are conducted using Appium"
             }
-            post
-            {
-                always
-                {
-                    //mail to: "jenkinsdeakin@gmail.com",
-                    //subject: "Unit tests and Integration tests outcome",
-                    //body: "Unit tests and Integration test have been successful"
-
-                    emailext body: 'Unit tests and Integration test have been successful',
-                    subject: 'Unit tests and Integration tests outcome',
-                    to: 'jenkinsdeakin@gmail.com'
-                    //attachLog: true
-
-                    //emailext body: 'Test Message',
-                    //subject: 'Test Subject',
-                    //to: 'jenkinsdeakin@gmail.com'
-
-                    //emailext(
-                      //subject: "Build ${currentBuild.fullDisplayName}",
-                      //body: "Build ${currentBuild.fullDisplayName} completed with status ${currentBuild.currentResult}",
-                      //to: "jenkinsdeakin@gmail.com")
-
+            post {
+                always {
+                    emailext (
+                        body: 'Unit tests and Integration test have been successful',
+                        subject: 'Unit tests and Integration tests outcome',
+                        to: 'jenkinsdeakin@gmail.com',
+                        attachLog: true  // Attach build log to email
+                    )
                 }
             }
-            
         }
-        stage('Code Analysis')
-        {
-            steps
-            {
+        stage('Code Analysis') {
+            steps {
                 echo "Analysing Code to ensure it meets industry standards."
                 echo "Code Analysed using SonarQube"
             }
-            
         }
-        stage('Security Scan')
-        {
-            steps
-            {
+        stage('Security Scan') {
+            steps {
                 echo "Conducting security scan"
                 echo "Security scan completed using Burp Suite"
             }
-            post
-            {
-                always
-                {
-                    //mail to: "jenkinsdeakin@gmail.com",
-                    //subject: "Security Scan outcome",
-                    //body: "Security Scan was successful"
-                    emailext body: 'Test Message',
-                    subject: 'Test Subject',
-                    to: 'jenkinsdeakin@gmail.com'
-                    //attachLog: true
-                    
-                    
+            post {
+                always {
+                    emailext (
+                        body: 'Security Scan was successful',
+                        subject: 'Security Scan outcome',
+                        to: 'jenkinsdeakin@gmail.com',
+                        attachLog: true  // Attach build log to email
+                    )
                 }
             }
         }
-        stage('Deploy to Staging')
-        {
-            steps
-            {
+        stage('Deploy to Staging') {
+            steps {
                 echo "Application will be deployed to staging server"
                 echo "Staging server is AWS EC2"
             }
         }
-        stage('Integration Tests on Staging')
-        {
-            steps
-            {
-                
-                 echo "Running integration tests on the staging environment to ensure the application functions as expected in a production-like environment."
-                
+        stage('Integration Tests on Staging') {
+            steps {
+                echo "Running integration tests on the staging environment to ensure the application functions as expected in a production-like environment."
             }
         }
-        stage('Deploy to Production')
-        {
-            steps
-            {
+        stage('Deploy to Production') {
+            steps {
                 echo "Deploying the application"
                 echo "Production Server used is AWS EC2"
             }
         }
-
     }
 
-        
-
     post {
-            always {
-                emailext (
-                    subject: "Pipeline Status: ${currentBuild.result}",
-                    body: '''<html>
-                                <body>
-                                    <p>Build Status: ${BUILD_STATUS}</p>
-                                    <p>Build Number: ${BUILD_NUMBER}</p>
-                                    <p>Check the <a href="${BUILD_URL}">console output</a>.</p>
-                                    
-                                </body>
-                            </html>''',
-                    to: 'jenkinsdeakin@gmail.com',
-                    from: 'jenkins@example.com', 
-                    replyTo: 'jenkins@example.com', 
-                    mimeType: 'text/html'
-                    )
-            }
+        always {
+            emailext (
+                subject: "Pipeline Status: ${currentBuild.result}",
+                body: '''<html>
+                            <body>
+                                <p>Build Status: ${BUILD_STATUS}</p>
+                                <p>Build Number: ${BUILD_NUMBER}</p>
+                                <p>Check the <a href="${BUILD_URL}">console output</a>.</p>
+                            </body>
+                        </html>''',
+                to: 'jenkinsdeakin@gmail.com',
+                from: 'jenkins@example.com', 
+                replyTo: 'jenkins@example.com', 
+                mimeType: 'text/html'
+            )
         }
-
+    }
+}
